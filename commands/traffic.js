21a74eb7648670c20275b2670cc0a040f47e4d8f
BliTz <60670940+BliTz037@users.noreply.github.com>
2021-02-05 22:23:56 +0100

    [ADD] !traffic multiple argument

--- commands/traffic.js
+++ commands/traffic.js
@@ -2,24 +2,27 @@
 const Discord = require('discord.js');
 const Api = require('../api');
 
-function checkStatusTrafic(type, name) {
+function checkStatusTrafic(type, name, DisplayLine) {
     const len = type.length;
-    let normal = 0;
-    let travaux = 0;
-    let critical = 0;
+    const normal = [];
+    const travaux = [];
+    const critical = [];
 
     for (const m of type) {
         if (m.slug === 'normal') {
-            normal++;
+            normal.push(m.line);
         }
         if (m.slug === 'normal_trav') {
-            travaux++;
+            travaux.push(m.line);
         }
         if (m.slug === 'critical') {
-            critical++;
+            critical.push(m.line);
         }
     }
-    return { name: `${len} ${name} :`, value: `:white_check_mark: OK : ${normal}\n:construction_worker: Travaux : ${travaux}\n:exclamation: Incidents : ${critical}` };
+    if (!DisplayLine) {
+        return { name: `${len} ${name} :`, value: `:white_check_mark: OK : ${normal.length}\n:construction_worker: Travaux : ${travaux.length}\n:exclamation: Incidents : ${critical.length}` };
+    }
+    return { name: `${len} ${name} :`, value: `:white_check_mark: OK : ${normal.length} (${normal.join(', ')})\n:construction_worker: Travaux : ${travaux.length} (${travaux.join(', ')})\n:exclamation: Incidents : ${critical.length} (${critical.join(', ')})` };
 }
 
 function checkStatusTraficOnlyType(type, name) {
@@ -39,7 +42,7 @@ function checkStatusTraficOnlyType(type, name) {
             critical.push(m.line);
         }
     }
-    return { name: `${len} ${name} :`, value: `:white_check_mark: OK : ${normal.toString()}\n:construction_worker: Travaux : ${travaux.toString()}\n:exclamation: Incidents : ${critical.toString()}` };
+    return { name: `${len} ${name} :`, value: `:white_check_mark: OK : ${normal.join(', ')}\n:construction_worker: Travaux : ${travaux.join(', ')}\n:exclamation: Incidents : ${critical.join(', ')}` };
 }
 
 module.exports = {
@@ -54,25 +57,39 @@ module.exports = {
         .setURL('https://www.ratp.fr/infos-trafic')
         .setAuthor('RATP Bot', 'https://zupimages.net/up/21/05/84vw.png', 'https://www.ratp.fr/')
         .setThumbnail('https://www.pngfactory.net/_png/_thumb/quille-travaux_cameleonhelp_divers.png');
+
         if (args.length === 0) {
             Api.get('traffic').then((resp) => {
-                embed.addFields(checkStatusTrafic(resp.result.metros, 'Metros :metro:'));
-                embed.addFields(checkStatusTrafic(resp.result.rers, 'RERS :train2:'));
-                embed.addFields(checkStatusTrafic(resp.result.tramways, 'Tramways :tram:'));
-                msg.channel.send(embed);
+                embed.addFields(checkStatusTrafic(resp.result.metros, 'Metros :metro:', false));
+                embed.addFields(checkStatusTrafic(resp.result.rers, 'RERS :train2:', true));
+                embed.addFields(checkStatusTrafic(resp.result.tramways, 'Tramways :tram:', true));
+                return msg.channel.send(embed);
             }).catch((reject) => {
                 console.log('FAIL');
                 console.log(reject);
+                return;
             });
         }
         else if (args.length === 1) {
             if (['metros', 'rers', 'tramways'].includes(args[0])) {
                 Api.get(`traffic/${args[0]}`).then((resp) => {
                     embed.addFields(checkStatusTraficOnlyType(resp.result[args[0]], args[0].charAt(0).toUpperCase() + args[0].slice(1)));
-                    msg.channel.send(embed);
+                    return msg.channel.send(embed);
                 }).catch((reject) => {
                     console.log('FAIL');
                     console.log(reject);
+                    return;
+                });
+            }
+        }
+        else if (args.length === 2) {
+            if (['metros', 'rers', 'tramways'].includes(args[0])) {
+                Api.get(`traffic/${args[0]}/${args[1]}`).then((resp) => {
+                    embed.setTitle(`TRAFFIC RATP : ${args[0]} Ligne ${args[1]}`);
+                    embed.addFields({ name: `Statut : ${resp.result.title}`, value: `${resp.result.message}` });
+                    return msg.channel.send(embed);
+                }).catch((reject) => {
+                    console.log(`Erreur ${reject.result.code} : La ligne ${args[1]} n'existe pas !`);
                 });
             }
         }
