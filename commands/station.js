966c4589518cc7141c3a9d145a5599e0014b3453
Tom Rives <tom.rives@epitech.eu>
2021-02-13 21:55:03 +0100

    [ADD] Command station

---
+++ commands/station.js
@@ -0,0 +1,46 @@
+/* eslint-disable indent */
+const Discord = require('discord.js');
+const Api = require('../api');
+const Url = require('../url.json');
+
+module.exports = {
+    name: 'station',
+    description: 'Obtenir la liste de toute les stations d\'une ligne',
+    aliases: ['station', 'stations', 'ligne'],
+    guildOnly: false,
+
+    execute(msg, args) {
+        let type = null
+        let line = null;
+        let str = '```\n';
+        let embed = new Discord.MessageEmbed();
+
+        if (args.length < 2) {
+            return msg.reply('Erreur: Il manque des arguments');
+        }
+        type = Api.checkType(args[0]);
+        if (type == null) {
+            return msg.reply('Correct usage:');
+        }
+        if (type === 'rers') {
+            line = args[1].toUpperCase();
+        }
+        else {
+            line = args[1].toLowerCase();
+        }
+        console.log("test");
+        Api.get(`stations/${type}/${line}`).then((resolve) => {
+            embed.setTitle(`Liste des stations du ${type} ${line}`);
+            embed.setThumbnail(Url.picto[`picto_${type}`]);
+            resolve.result.stations.forEach(element => {
+                str += `- ${element.name}\n`;
+            });
+            str += '```'
+            embed.addFields({name: `${resolve.result.stations.length} stations`, value: str});
+            return msg.channel.send(embed);
+        }).catch((err) => {
+            console.log(err);
+            return msg.reply(`Erreur ${err.result.code}: La ligne de ${type} renseignée n'existe pas !`);
+        });
+    }
+}
